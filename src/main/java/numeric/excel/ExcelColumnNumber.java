138b5edfba7e7b7fd3d2731cd018b997dec32865
Pasha Kuznetsov <pasha@tydbits.com>
2017-09-26 20:02:30 -0500

    Excel Column Name by it Number

---
+++ src/main/java/numeric/excel/ExcelColumnNumber.java
@@ -0,0 +1,27 @@
+package numeric.excel;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+
+// Accept an integer as input and output the corresponding column
+// the number is represented in in Excel. So, 1 is A, 27 is AA, etc.
+class ExcelColumnNumber {
+    final static char first = 'A';
+    final static char last = 'Z';
+    final static int radix = 'Z' - 'A' + 1;
+
+    static String toString(int n) {
+        StringBuilder result = new StringBuilder();
+        for (; n > 0; n /= radix) {
+            int d = n % radix;
+            if (d == 0) {
+                result.append(last);
+                n--;
+            }
+            else {
+                result.append((char)(first + d - 1));
+            }
+        }
+        return result.reverse().toString();
+    }
+}
---
+++ src/test/groovy/numeric/excel/ExcelColumnNumber_Test.groovy
@@ -0,0 +1,25 @@
+package numeric.excel
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class ExcelColumnNumber_Test extends Specification {
+    @Unroll
+    def "test #n"() {
+        expect:
+        ExcelColumnNumber.toString(n) == expectedOutput
+
+        where:
+        n   || expectedOutput
+        0   || ''
+        1   || 'A'      // % 26 -> 1  'A'
+        26  || 'Z'      // % 26 -> 0  'Z',  - 1, / 26 -> 0
+        27  || 'AA'     // % 26 -> 1  'A',       / 26 -> 1   % 26 -> 1 'A'
+        51  || 'AY'     // % 26 -> 25 'Y',       / 26 -> 1   % 26 -> 1 'A'
+        52  || 'AZ'     // % 26 -> 0  'Z',  - 1, / 26 -> 1   % 26 -> 1 'A'
+        80  || 'CB'
+        676 || 'YZ'
+        702 || 'ZZ'     // % 26 -> 0  'Z',  - 1, / 26 -> 26  % 26 -> 0 -> 'Z'   - 1, / 26 = 0
+        705 || 'AAC'
+    }
+}
