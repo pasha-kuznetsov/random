380139e17c2203d1ab5077310bcc2a928d296539
Pasha Kuznetsov <pasha@tydbits.com>
2017-09-26 23:17:20 -0500

    Sorted array unique values: fix for odd number of non-unique values

--- src/main/java/arrays/SortedArrayUniqueValues.java
+++ src/main/java/arrays/SortedArrayUniqueValues.java
@@ -9,10 +9,13 @@ public class SortedArrayUniqueValues {
     static List<Integer> uniqueValues(int[] array) {
         List<Integer> unique = new ArrayList<>();
         for (int i = 0; i < array.length; ) {
-            if (i + 1 == array.length || array[i] != array[i + 1])
+            if (i + 1 == array.length || array[i] != array[i + 1]) {
                 unique.add(array[i++]);
-            while (i + 1 < array.length && array[i] == array[i + 1])
-                i += 2;
+            } else {
+                int n = array[i++];
+                while (i < array.length && array[i] == n)
+                    ++i;
+            }
         }
         return unique;
     }
--- src/test/groovy/arrays/SortedArrayUniqueValues_Test.groovy
+++ src/test/groovy/arrays/SortedArrayUniqueValues_Test.groovy
@@ -10,7 +10,7 @@ class SortedArrayUniqueValues_Test extends Specification {
         SortedArrayUniqueValues.uniqueValues(array as int[]) == unique
 
         where:
-        array                       | unique
-        [0, 1, 2, 3, 3, 4, 5, 5, 6] | [0, 1, 2, 4, 6]
+        array                          | unique
+        [0, 1, 2, 3, 3, 4, 5, 5, 5, 6] | [0, 1, 2, 4, 6]
     }
 }
